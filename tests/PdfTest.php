1485b19052249f3bdabb6a22478b9b761a9889ac
James Baxter <jamesdb@users.noreply.github.com>
2016-04-13 11:00:10 +0100

    Added getImageData (#4)
    * Added getImageData
    
    * Padded out the test suite
    
    * Attempt to install Imagick on travis VM
    
    * Made test pass
    
    * Allow failures on hhvm

--- tests/PdfTest.php
+++ tests/PdfTest.php
@@ -3,6 +3,7 @@
 namespace Spatie\PdfToImage\Test;
 
 use Spatie\PdfToImage\Exceptions\InvalidFormat;
+use Spatie\PdfToImage\Exceptions\PageDoesNotExist;
 use Spatie\PdfToImage\Exceptions\PdfDoesNotExist;
 use Spatie\PdfToImage\Pdf;
 
@@ -18,10 +19,11 @@ class PdfTest extends \PHPUnit_Framework_TestCase
         parent::setUp();
 
         $this->testFile = __DIR__.'/files/test.pdf';
+        $this->multipageTestFile = __DIR__.'/files/multipage-test.pdf';
     }
 
     /** @test */
-    public function it_will_throw_an_exception_when_try_to_convert_an_non_existing_file()
+    public function it_will_throw_an_exception_when_try_to_convert_a_non_existing_file()
     {
         $this->setExpectedException(PdfDoesNotExist::class);
 
@@ -34,5 +36,61 @@ class PdfTest extends \PHPUnit_Framework_TestCase
         $this->setExpectedException(InvalidFormat::class);
 
         (new Pdf($this->testFile))->setOutputFormat('bla');
+    }
+
+    /** @test */
+    public function it_will_throw_an_exception_when_passed_an_invalid_page()
+    {
+        $this->setExpectedException(PageDoesNotExist::class);
+
+        (new Pdf($this->testFile))->setPage(5);
+    }
+
+    /** @test */
+    public function it_will_correctly_return_the_number_of_pages_in_pdf_file()
+    {
+        $pdf = new Pdf($this->multipageTestFile);
+
+        $this->assertTrue($pdf->getNumberOfPages() === 3);
+    }
+
+    /** @test */
+    public function it_will_accept_a_custom_specified_resolution()
+    {
+        $pdf = new Pdf($this->testFile);
+
+        $pdf->setResolution(72);
+
+        $image = $pdf->getImageData('test.jpg')->getImageResolution();
+
+        $this->assertEquals($image['x'], 72);
+        $this->assertEquals($image['y'], 72);
+        $this->assertNotEquals($image['x'], 144);
+        $this->assertNotEquals($image['y'], 144);
+    }
+
+    /** @test */
+    public function it_will_convert_a_specified_page()
+    {
+        $pdf = new Pdf($this->multipageTestFile);
+
+        $pdf->setPage(2);
+
+        $imagick = $pdf->getImageData('page-2.jpg');
+
+        $this->assertInstanceOf('Imagick', $imagick);
+    }
+
+    /** @test */
+    public function it_will_accept_a_specified_file_type_and_convert_to_it()
+    {
+        $pdf = new pdf($this->testFile);
+
+        $pdf->setOutputFormat('png');
+
+        $imagick = $pdf->getImageData('test.png');
+
+        $this->assertSame($imagick->getFormat(), 'png');
+        $this->assertNotSame($imagick->getFormat(), 'jpg');
     }
 }
