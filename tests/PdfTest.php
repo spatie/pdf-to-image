94fcd285c8ddd00afbcdebf09aed3a33c328d390
peter279k <peter279k@gmail.com>
2021-01-27 03:22:33 +0800

    Improve PHPUnit fixture

--- tests/PdfTest.php
+++ tests/PdfTest.php
@@ -20,7 +20,7 @@ class PdfTest extends TestCase
     /** @var string */
     protected $remoteFileUrl;
 
-    public function setUp(): void
+    protected function setUp(): void
     {
         parent::setUp();
 
