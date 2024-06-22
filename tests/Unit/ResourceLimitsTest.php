557c5098a5070356296afbccfcb84b02ae5d0fd9
Patrick Organ <patrick@permafrost.dev>
2024-06-22 10:04:29 -0400

    add unit tests

---
+++ tests/Unit/ResourceLimitsTest.php
@@ -0,0 +1,70 @@
+<?php
+
+use Spatie\PdfToImage\Pdf;
+use Spatie\PdfToImage\Enums\ResourceLimitType;
+
+it('sets the area resource limit', function () {
+    $pdf = new Pdf($this->testFile);
+    $im = $pdf->resourceLimit(ResourceLimitType::Area, 1024 * 1024 * 16)
+        ->getImageData($this->testFile, 1);
+
+    expect((int)$im::getResourceLimit(ResourceLimitType::Area->value))->toBe((int)1024*1024*16);
+});
+
+it('sets the disk resource limit', function () {
+    $pdf = new Pdf($this->testFile);
+    $im = $pdf->resourceLimit(ResourceLimitType::Disk, 1024 * 1024)
+        ->getImageData($this->testFile, 1);
+
+    expect((int)$im::getResourceLimit(ResourceLimitType::Disk->value))->toBe((int)1024*1024);
+});
+
+it('sets the file resource limit', function () {
+    $pdf = new Pdf($this->testFile);
+    $im = $pdf->resourceLimit(ResourceLimitType::File, 5)
+        ->getImageData($this->testFile, 1);
+
+    expect((int)$im::getResourceLimit(ResourceLimitType::File->value))->toBe((int)5);
+});
+
+
+it('sets the map resource limit', function () {
+    $pdf = new Pdf($this->testFile);
+    $im = $pdf->resourceLimit(ResourceLimitType::Map, 1024 * 1024*16)
+        ->getImageData($this->testFile, 1);
+
+    expect((int)$im::getResourceLimit(ResourceLimitType::Map->value))->toBe((int)1024*1024*16);
+});
+
+it('sets the memory resource limit', function () {
+    $pdf = new Pdf($this->testFile);
+    $im = $pdf->resourceLimit(ResourceLimitType::Memory, 1024 * 1024*32)
+        ->getImageData($this->testFile, 1);
+
+    expect((int)$im::getResourceLimit(ResourceLimitType::Memory->value))->toBe((int)1024*1024*32);
+});
+
+it('sets the time resource limit', function () {
+    $pdf = new Pdf($this->testFile);
+    $im = $pdf->resourceLimit(ResourceLimitType::Time, 10)
+        ->getImageData($this->testFile, 1);
+
+    expect((int)$im::getResourceLimit(ResourceLimitType::Time->value))->toBe((int)10);
+});
+
+it('sets the throttle resource limit', function () {
+    $pdf = new Pdf($this->testFile);
+    $im = $pdf->resourceLimit(ResourceLimitType::Throttle, 10)
+        ->getImageData($this->testFile, 1);
+
+    expect((int)$im::getResourceLimit(ResourceLimitType::Throttle->value))->toBe((int)10);
+});
+
+it('sets the thread resource limit', function () {
+    $pdf = new Pdf($this->testFile);
+    $im = $pdf->resourceLimit(ResourceLimitType::Thread, 4)
+        ->getImageData($this->testFile, 1);
+
+    expect((int)$im::getResourceLimit(ResourceLimitType::Thread->value))->toBe((int)4);
+});
+
