2ba39858dac8e2d06fc010d7ad86ead03c18aebb
patinthehat <5508707+patinthehat@users.noreply.github.com>
2025-05-16 04:01:30 +0000

    Fix styling

--- tests/Pest.php
+++ tests/Pest.php
@@ -4,18 +4,17 @@ use Spatie\PdfToImage\Test\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
 
-
 function get_test_file(string $name, ?string $extension = null, string $dirname = 'files'): string
 {
     $extension = empty($extension) ? pathinfo($name, PATHINFO_EXTENSION) : $extension;
     $name = pathinfo($name, PATHINFO_FILENAME);
-    $extension = ltrim($extension,'.');
+    $extension = ltrim($extension, '.');
 
     if (empty($extension)) {
         $extension = 'pdf';
     }
 
-    return __DIR__ . "/$dirname/test-$name.$extension";
+    return __DIR__."/$dirname/test-$name.$extension";
 }
 
 function test_file(string $name, ?string $extension = null): string
